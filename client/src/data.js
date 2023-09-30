fc296bdd4c1026a3da065c194d6023f3bd47a0cb
omarfrikha96 <omar.frikha96@gmail.com>
2023-09-30 16:50:26 +0100

    Prepare first look of the site

---
+++ client/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+        name: 'Adidas Slim shirt',
+        slug: 'adidas-slim-shirt',
+        category: 'Shirts',
+        image: '/images/2.jpg',
+        price: 130,
+        countInStock: 9,
+        brand: 'Adidas',
+        rating: 5,
+        numReviews: 11,
+        description: 'high quality shirt',
+      },
+      {
+        name: 'Puma Slim shirt',
+        slug: 'puma-slim-shirt',
+        category: 'Shirts',
+        image: '/images/3.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'Puma',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality shirt',
+      },
+      {
+        name: 'Puma Slim pant',
+        slug: 'puma-slim-pant',
+        category: 'Pants',
+        image: '/images/4.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'Puma',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality shirt',
+      }
+  ],
+};
+
+export default data;
