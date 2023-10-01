971a4e10bdf9d4bda424c270b0d1325e27aca35d
omarfrikha96 <omar.frikha96@gmail.com>
2023-10-01 14:24:26 +0100

    Seed-Sample-Users

--- server/routes/seed.routes.js
+++ server/routes/seed.routes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import Product from '../models/product.model.js';
+import User from '../models/user.model.js';
 import data from '../data.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
-  await Product.deleteMany();
-
+  await Product.deleteMany({});
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+  await User.deleteMany({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts,createdUsers });
 });
 export default seedRouter;
