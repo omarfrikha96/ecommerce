c06cc78df14dea28d0256953873f365f7987ea0f
omarfrikha96 <omar.frikha96@gmail.com>
2023-10-02 23:44:09 +0100

    Implement-Place-Order-Action

--- client/src/Store.js
+++ client/src/Store.js
@@ -41,6 +41,8 @@ function reducer(state, action) {
       localStorage.setItem('cartItems', JSON.stringify(cartItems)); //store in local storage type script with key cartItems
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case 'CART_CLEAR':
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
     case 'USER_SIGNIN':
       return { ...state, userInfo: action.payload };
     case 'USER_SIGNOUT':
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import seedRouter from './routes/seed.routes.js';
 import productRouter from './routes/product.routes.js';
 import userRouter from './routes/user.routes.js';
+import orderRouter from './routes/order.routes.js';
 
 
 dotenv.config();
@@ -25,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
-
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
